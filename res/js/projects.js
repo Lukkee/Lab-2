0688bc464f0d28046bcfa8f4f8990a9deb09527b
Lukas Lundin <lukas@holy.nu>
2025-12-25 16:32:22 +0100

    Added more projects, changed loadCards() to a loop

--- res/js/projects.js
+++ res/js/projects.js
@@ -10,26 +10,47 @@ class project {
     }
 }
 
-const project1 = new project(
+const fractal_project = new project(
     0, 
     "Fractal Generator",
     "A Fractal generator made using js and presented in html/css, made as a hobby project. The generator uses L-Systems to generatively produce fractals through multiple generations.",
     "Hobby Project",
     ["Javascript", "HTML", "CSS"],
     "./res/img/fractal.jpg",
-    "http://luklun.se/fractal.html"
+    "https://github.com/Lukkee/fractal-generation-using-L-systems"
 );
 
-const project2 = new project(
+const p5_project = new project(
     1, 
     "P5.js editor",
     "A P5.js web-editor made as a hobby project, using codemirror to let the user input code in a window and view the result in a canvas.",
     "Hobby Project",
     ["Javascript", "HTML", "CSS"],
     "./res/img/p5.png",
-    "http://luklun.se/p5editor.html"
+    "https://github.com/Lukkee/p5-editor-using-codemirror"
 );
 
+const http_server_project = new project(
+    2,
+    "Simple HTTP Server",
+    "A simple HTTP server made using C, with ARPA and INET libraries. Made as a hobby project to learn more about networking and HTTP protocol as well as host local projects.",
+    "Hobby Project",
+    ["C", "ARPA", "INET", "HTTP"],
+    "./res/img/default.png",
+    "https://github.com/Lukkee/HTTP-server"
+);
+
+const desklet_project = new project(
+    3,
+    "Command Desklet for Cinnamon",
+    "A simple linux Cinnamon desklet made using js, for showing command outputs in Cinnamon 6.4.X.",
+    "Open Source Hobby Project",
+    ["Javascript", "JSON"],
+    "./res/img/default.png",
+    "https://github.com/Lukkee/commands-desklet"
+);
+
+const projects = [fractal_project, p5_project, http_server_project, desklet_project];
 
 const container = document.getElementById("projects-container");
 
@@ -86,8 +107,9 @@ function buildProjectCard(project) {
 }
 
 function loadCards() {
-    container.appendChild(buildProjectCard(project1));
-    container.appendChild(buildProjectCard(project2));
+    for (const project of projects) {
+        container.appendChild(buildProjectCard(project));
+    }
 }
 
 loadCards();
